267c49dfabb2e7ecdd10900114746cf63d7368bb
ahrenholz <siliconja@users.noreply.github.com>
2014-10-31 16:45:03 +0000

    added Docker service contributed by Stuart Marsden, with some mods fixes bug #264 (Boeing r1896)

--- daemon/core/services/__init__.py
+++ daemon/core/services/__init__.py
@@ -3,4 +3,4 @@
 Services available to nodes can be put in this directory.  Everything listed in
 __all__ is automatically loaded by the main core module.
 """
-__all__ = ["quagga", "nrl", "xorp", "bird", "utility", "security", "ucarp"]
+__all__ = ["quagga", "nrl", "xorp", "bird", "utility", "security", "ucarp", "dockersvc"]
