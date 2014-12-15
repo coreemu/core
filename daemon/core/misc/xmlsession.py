104e68a7125d0c9020bb6026913d5763de5701b2
tgoff0 <tgoff0@gmail.com>
2014-12-15 18:22:53 +0000

    daemon: Initial support to allow multiple versions of XML scenario files.

--- daemon/core/misc/xmlsession.py
+++ daemon/core/misc/xmlsession.py
@@ -1,6 +1,6 @@
 #
 # CORE
-# Copyright (c)2011-2013 the Boeing Company.
+# Copyright (c)2011-2014 the Boeing Company.
 # See the LICENSE file included in this distribution.
 #
 # author: Jeff Ahrenholz <jeffrey.m.ahrenholz@boeing.com>
@@ -13,13 +13,14 @@ the main public interface here.
 
 import os.path
 from core.netns import nodes
-from xmlparser import CoreDocumentParser
+from xmlparser import core_document_parser
 from xmlwriter import CoreDocumentWriter
 
 def opensessionxml(session, filename, start=False, nodecls=nodes.CoreNode):
     ''' Import a session from the EmulationScript XML format.
     '''
-    doc = CoreDocumentParser(session, filename, start, nodecls)
+    options = {'start': start, 'nodecls': nodecls}
+    doc = core_document_parser(session, filename, options)
     if start:
         session.name = os.path.basename(filename)
         session.filename = filename
