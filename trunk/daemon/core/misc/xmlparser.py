0b61877b0bebae7424026ae9a0706d6e31b225b7
tgoff0@gmail.com <tgoff0@gmail.com@286e914e-1314-11de-a46f-cd5bcf99231e>
2015-02-05 00:15:59 +0000

    daemon: Renumber XML versioning.
    Consider the current XML representation version 0.0.

--- trunk/daemon/core/misc/xmlparser.py
+++ trunk/daemon/core/misc/xmlparser.py
@@ -4,13 +4,13 @@
 
 from xml.dom.minidom import parse
 from xmlutils import getoneelement
-from xmlparser1 import CoreDocumentParser1
+from xmlparser0 import CoreDocumentParser0
 
 class CoreVersionParser(object):
     '''\
     Helper class to check the version of Network Plan document.  This
     simply looks for a "Scenario" element; when present, this
-    indicates a 1.0 version document.  The dom member is set in order
+    indicates a 0.0 version document.  The dom member is set in order
     to prevent parsing a file twice (it can be passed to the
     appropriate CoreDocumentParser class.)
     '''
@@ -21,7 +21,7 @@ class CoreVersionParser(object):
             self.dom = parse(filename)
         self.scenario = getoneelement(self.dom, 'Scenario')
         if self.scenario is not None:
-            self.version = 1.0
+            self.version = 0.0
         else:
             self.version = 'unknown'
 
@@ -29,8 +29,8 @@ def core_document_parser(session, filename, options):
     vp = CoreVersionParser(filename, options)
     if 'dom' not in options:
         options['dom'] = vp.dom
-    if vp.version == 1.0:
-        doc = CoreDocumentParser1(session, filename, options)
+    if vp.version == 0.0:
+        doc = CoreDocumentParser0(session, filename, options)
     else:
         raise ValueError, 'unsupported document version: %s' % vp.version
     return doc
