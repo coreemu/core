00a2fdaecba324d8c91c457a43532570e41dfba6
tgoff0@gmail.com <tgoff0@gmail.com@286e914e-1314-11de-a46f-cd5bcf99231e>
2015-02-19 18:12:44 +0000

    Improve python install prefix determination.

--- trunk/python-prefix.py
+++ trunk/python-prefix.py
@@ -5,19 +5,30 @@ import os.path
 import site
 
 def main():
-    if len(sys.argv) != 2:
-        msg = 'usage: %s <prefix>\n' % os.path.basename(sys.argv[0])
+    '''\
+    Check if the given prefix is included in sys.path for the given
+    python version; if not find an alternate valid prefix.  Print the
+    result to standard out.
+    '''
+    if len(sys.argv) != 3:
+        msg = 'usage: %s <prefix> <python version>\n' % \
+              os.path.basename(sys.argv[0])
         sys.stderr.write(msg)
         return 1
     python_prefix = sys.argv[1]
+    python_version = sys.argv[2]
+    path = '%s/lib/python%s' % (python_prefix, python_version)
+    path = os.path.normpath(path)
+    if path[-1] != '/':
+        path = path + '/'
     prefix = None
     for p in sys.path:
-        if python_prefix in p:
-            prefix = python_prefix
+        if p.startswith(path):
+            prefix = path
             break
     if not prefix:
         prefix = site.PREFIXES[-1]
-    print prefix
+    sys.stdout.write('%s\n' % prefix)
     return 0
 
 if __name__ == '__main__':
