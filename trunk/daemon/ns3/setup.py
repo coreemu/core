73d5b63f729141394f9ac9b9b155d7cb1061e3bc
tgoff0@gmail.com <tgoff0@gmail.com@286e914e-1314-11de-a46f-cd5bcf99231e>
2015-05-26 17:33:57 +0000

    daemon: Use automake to install python data files.
    Includes other small changes.

--- trunk/daemon/ns3/setup.py
+++ trunk/daemon/ns3/setup.py
@@ -5,20 +5,11 @@ import os, glob
 from distutils.core import setup
 from corens3.constants import COREDPY_VERSION
 
-# optionally pass CORE_CONF_DIR using environment variable
-confdir = os.environ.get('CORE_CONF_DIR')
-if confdir is None:
-    confdir="/etc/core"
-
 setup(name = "corens3-python",
       version = COREDPY_VERSION,
       packages = [
         "corens3",
         ],
-      data_files = [
-                    ("share/core/examples/corens3",
-                     glob.glob("examples/*.py")),
-                    ],
       description = "Python ns-3 components of CORE",
       url = "http://www.nrl.navy.mil/itd/ncs/products/core",
       author = "Boeing Research & Technology",
