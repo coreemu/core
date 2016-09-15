928617e613ab61a84fa25a0bcc675d8783421487
Rod A Santiago <rodolfo.a.santiago@boeing.com>
2016-09-14 17:12:25 -0700

    Merged new versions from NRL

--- daemon/core/netns/vnet.py
+++ daemon/core/netns/vnet.py
@@ -11,8 +11,6 @@ vnet.py: PyCoreNet and LxBrNet classes that implement virtual networks using
 Linux Ethernet bridging and ebtables rules.
 '''
 
-import traceback
-
 import os, sys, threading, time, subprocess
 
 from core.api import coreapi
@@ -347,7 +345,6 @@ class LxBrNet(PyCoreNet):
         ''' Configure link parameters by applying tc queuing disciplines on the
             interface.
         '''
-        
         if devname is None:
             devname = netif.localname
         tc = [TC_BIN, "qdisc", "replace", "dev", devname]
