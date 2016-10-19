ca4befac587ca6010c6d025ef448cc63864590c7
andrew <andrew.ahlers@gmail.com>
2016-10-18 21:21:07 -0400

    Now meets flake8 standards

--- daemon/setup.py
+++ daemon/setup.py
@@ -1,13 +1,14 @@
 # Copyright (c)2010-2012 the Boeing Company.
 # See the LICENSE file included in this distribution.
 
-import os, glob
+# import os
+# import glob
 from distutils.core import setup
 from core.constants import COREDPY_VERSION
 
-setup(name = "core-python",
-      version = COREDPY_VERSION,
-      packages = [
+setup(name="core-python",
+      version=COREDPY_VERSION,
+      packages=[
         "core",
         "core.addons",
         "core.api",
@@ -19,10 +20,10 @@ setup(name = "core-python",
         "core.xen",
         "core.services",
         ],
-      description = "Python components of CORE",
-      url = "http://www.nrl.navy.mil/itd/ncs/products/core",
-      author = "Boeing Research & Technology",
-      author_email = "core-dev@pf.itd.nrl.navy.mil",
-      license = "BSD",
-      long_description="Python scripts and modules for building virtual " \
+      description="Python components of CORE",
+      url="http://www.nrl.navy.mil/itd/ncs/products/core",
+      author="Boeing Research & Technology",
+      author_email="core-dev@pf.itd.nrl.navy.mil",
+      license="BSD",
+      long_description="Python scripts and modules for building virtual " +
           "emulated networks.")
