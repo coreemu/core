afb6af5f87bf185d08088eebc9b8212c57ca4b26
Blake J. Harnden <bharnden@users.noreply.github.com>
2018-01-03 14:40:45 -0800

    removed old olog format line

--- daemon/core/__init__.py
+++ daemon/core/__init__.py
@@ -5,10 +5,12 @@ import os
 
 from core import constants
 
+
+# setup logging
 log_config_path = os.path.join(constants.CORE_CONF_DIR, "logging.conf")
 with open(log_config_path, "r") as log_config_file:
     log_config = json.load(log_config_file)
     logging.config.dictConfig(log_config)
 
-#logging.basicConfig(format="%(asctime)s - %(levelname)s - %(module)s:%(funcName)s - %(message)s", level=logging.DEBUG)
 logger = logging.getLogger()
+
