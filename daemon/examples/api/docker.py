8397d0c164f11f1a965714911e242a3fa8da9888
stuartmarsden <stuartmarsden@gmail.com>
2018-05-26 12:02:15 +0400

    Kind of working docker network node and example.

--- daemon/examples/api/docker.py
+++ daemon/examples/api/docker.py
@@ -12,11 +12,13 @@ from core.emulator.coreemu import CoreEmu
 from core.emulator.emudata import IpPrefixes
 from core.enumerations import NodeTypes, EventTypes
 from core.netns import nodes
+from core.misc import utils
 
 
-def example(options):
+def example():
     # ip generator for example
-    prefixes = IpPrefixes(ip4_prefix="10.83.0.0/16")
+    prefixes = IpPrefixes(ip4_prefix="10.0.0.0/16")
+    docker_net = "docker1"
 
     # create emulator instance for creating sessions and utility methods
     coreemu = CoreEmu()
@@ -27,47 +29,29 @@ def example(options):
 
     node1 = session.add_object(cls=nodes.CoreNode, name="n1")
     node2 = session.add_object(cls=nodes.CoreNode, name="n2")
-    dock1 = session.add_object(cls=nodes.DockerNetNode, name="docker1")
-    node1.newnetif(dock1, ["10.0.0.1/24"])
-    node2.newnetif(dock1, ["10.0.0.2/24"])
+    dock1 = session.add_object(cls=nodes.DockerNetNode, name=docker_net, subnet=str(prefixes.ip4))
+    node1_net = node1.newnetif(dock1, prefixes.create_interface(node1).ip4_address())
+    node2.newnetif(dock1, prefixes.create_interface(node2).ip4_address())
+    docker_id = utils.check_cmd(["docker", "run", "-d", "--net="+docker_net, "--ip="+str(prefixes.ip4.min_addr() + 1),
+                    "nginx"])
 
-    node1.client.icmd(["ping", "-c", "5", "10.0.0.2"])
+    print "Going to request web with no network effects"
+    start = datetime.datetime.now()
+    node1.client.icmd(["wget", "-O", "-", str(prefixes.ip4.min_addr() + 1)])
+    print "elapsed time: %s" % (datetime.datetime.now() - start)
 
+    print "Going to request web with no network effects"
+    start = datetime.datetime.now()
+    node1.client.icmd(["wget", "-O", "-", str(prefixes.ip4.min_addr() + 1)])
+    print "elapsed time: %s" % (datetime.datetime.now() - start)
 
-    # # create switch network node
-    # switch = session.add_node(_type=NodeTypes.SWITCH)
-    #
-    # # create nodes
-    # for _ in xrange(options.nodes):
-    #     node = session.add_node()
-    #     interface = prefixes.create_interface(node)
-    #     session.add_link(node.objid, switch.objid, interface_one=interface)
-    #
-    # # instantiate session
-    # session.instantiate()
-    #
-    # # get nodes to run example
-    # first_node = session.get_object(2)
-    # last_node = session.get_object(options.nodes + 1)
-    #
-    # print "starting iperf server on node: %s" % first_node.name
-    # first_node.cmd(["iperf", "-s", "-D"])
-    # first_node_address = prefixes.ip4_address(first_node)
-    # print "node %s connecting to %s" % (last_node.name, first_node_address)
-    # last_node.client.icmd(["iperf", "-t", str(options.time), "-c", first_node_address])
-    # first_node.cmd(["killall", "-9", "iperf"])
-
-    # shutdown session
+    utils.check_cmd(["docker", "rm", "-f", docker_id])
     coreemu.shutdown()
 
 
 def main():
-    options = parser.parse_options("switch")
 
-    start = datetime.datetime.now()
-    print "running switch example: nodes(%s) time(%s)" % (options.nodes, options.time)
-    example(options)
-    print "elapsed time: %s" % (datetime.datetime.now() - start)
+    example()
 
 
 if __name__ == "__main__":
