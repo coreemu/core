73b147b1520a2eed0f73d3411575932af1129299
Blake Harnden <32446120+bharnden@users.noreply.github.com>
2019-11-05 16:56:47 -0800

    updated tk servers dialog to save config

--- coretk/coretk/dialogs/servers.py
+++ coretk/coretk/dialogs/servers.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+from coretk import appdirs
 from coretk.coreclient import CoreServer
 from coretk.dialogs.dialog import Dialog
 
@@ -113,7 +114,14 @@ class ServersDialog(Dialog):
         button.grid(row=0, column=1, sticky="ew")
 
     def click_save_configuration(self):
-        pass
+        servers = []
+        for name in sorted(self.app.core.servers):
+            server = self.app.core.servers[name]
+            servers.append(
+                {"name": server.name, "address": server.address, "port": server.port}
+            )
+        self.app.config["servers"] = servers
+        appdirs.save_config(self.app.config)
 
     def click_create(self):
         name = self.name.get()
