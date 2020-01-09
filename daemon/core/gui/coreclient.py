3ef87a97d5ad84fb7cda8bf4cf089c356d533067
Huy Pham <42948410+hpham@users.noreply.github.com>
2020-01-09 10:12:00 -0800

    remove extra function

--- daemon/core/gui/coreclient.py
+++ daemon/core/gui/coreclient.py
@@ -933,6 +933,3 @@ class CoreClient:
             config = self.emane_model_configs.get(_from)
             if config:
                 self.emane_model_configs[_to] = config
-
-    def get_service_commands(self):
-        self.create_nodes_and_links()
