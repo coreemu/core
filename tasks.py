626b977505719d23d484a690f745f21101b34fd4
Blake Harnden <32446120+bharnden@users.noreply.github.com>
2020-07-10 11:30:12 -0700

    added ospf mdr redhat dependencies to invoke install

--- tasks.py
+++ tasks.py
@@ -117,6 +117,8 @@ def install_ospf_mdr(c: Context, os_info: OsInfo, hide: bool) -> None:
     print("installing ospf mdr dependencies...")
     if os_info.like == OsLike.DEBIAN:
         c.run("sudo apt install -y libtool gawk libreadline-dev", hide=hide)
+    elif os_info.like == OsLike.REDHAT:
+        c.run("sudo yum install -y libtool gawk readline-devel", hide=hide)
     print("cloning ospf mdr...")
     clone_dir = "/tmp/ospf-mdr"
     c.run(
