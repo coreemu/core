a201fd2903218c624fb1ef73671fbe084259fdea
Blake Harnden <32446120+bharnden@users.noreply.github.com>
2022-07-29 14:03:45 -0700

    install: add option to avoid y/n prompt when uninstalling core during post install script

--- package/after-remove.sh
+++ package/after-remove.sh
@@ -5,9 +5,9 @@ fi
 
 PYTHON="${PYTHON:=python3}"
 if [ -v NO_VENV ]; then
-  ${PYTHON} -m pip uninstall core
+  ${PYTHON} -m pip uninstall -y core
 else
   ${PYTHON} -m venv /opt/core/venv
   . /opt/core/venv/bin/activate
-  pip uninstall core
+  pip uninstall -y core
 fi
