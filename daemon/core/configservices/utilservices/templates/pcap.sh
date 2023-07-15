12c07e01753d1001b6d0dd4771d3c17a32fe72be
Blake Harnden <32446120+bharnden@users.noreply.github.com>
2023-07-15 10:27:41 -0700

    services: fixed pcap service not properly backgrounding command to avoid blocking

--- daemon/core/configservices/utilservices/templates/pcap.sh
+++ daemon/core/configservices/utilservices/templates/pcap.sh
@@ -3,7 +3,7 @@
 # (-s snap length, -C limit pcap file length, -n disable name resolution)
 if [ "x$1" = "xstart" ]; then
 % for ifname in ifnames:
-    tcpdump -s 12288 -C 10 -n -w ${node.name}.${ifname}.pcap -i ${ifname} < /dev/null &
+    tcpdump -s 12288 -C 10 -n -w ${node.name}.${ifname}.pcap -i ${ifname} > /dev/null 2>&1 &
 % endfor
 elif [ "x$1" = "xstop" ]; then
     mkdir -p $SESSION_DIR/pcap
